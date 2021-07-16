ffcd192d06b48b0d87ded4672e8cbe34f9495078
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-16 13:20:10 -0500

    fixed something

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -21,12 +21,15 @@ test('I can start a game', async () => {
 test('I can click top right', async () => {
     let upRight = await (await driver).findElement(By.id('cell-2'));
     await upRight.click();
+    expect(await upRight.getText()).toEqual('X');
 })
 test('I can click top left', async () => {
     let upLeft = await (await driver).findElement(By.id('cell-0'));
     await upLeft.click();
+    expect(await upLeft.getText()).toEqual('X');
 })
 test('I can click bottom right', async () => {
     let bottomRight = await (await driver).findElement(By.id('cell-8'));
     await bottomRight.click();
+    expect(await bottomRight.getText()).toEqual('X');
 })
